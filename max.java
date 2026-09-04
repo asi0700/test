7f3fd76ca162d831d973db5f0f63e2da853b5e2a
asi0700 <asisanginov@gmail.com>
2026-09-04 14:00:45 +0300

    добавление альтернативных способов решение этой задачи

--- max.java
+++ max.java
@@ -19,5 +19,24 @@ public class max {
         max = (max > c) ? max : c;
         max = (max > d) ? max : d;
         System.out.println(" maximum : " + max);
+
+        //альтернативный способ через метод Math.max
+        //int max = Math.max(Math.max(a, b), Math.max(c, d));
+        //System.out.println(" maximum : " + max);
+
+
+        // или еще один способ через массив
+        //int[] numbers = {a, b, c, d};
+
+        // или еще один способ через условие 
+        //if (a > b && a > c && a > d) {
+            //System.out.println(" maximum : " + a);
+        //} else if (b > a && b > c && b > d) {
+            //System.out.println(" maximum : " + b);
+        //} else if (c > a && c > b && c > d) {
+            //System.out.println(" maximum : " + c);
+        //} else {
+            //System.out.println(" maximum : " + d);
+        //}
     }
 }

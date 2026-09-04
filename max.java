176a603cb1fa94fe8ecc1ba27022a975f4b2655d
asi0700 <asisanginov@gmail.com>
2026-09-04 13:38:40 +0300

    add file max.java

---
+++ max.java
@@ -0,0 +1,9 @@
+public class max {
+    public static void main(String[] args) {
+     
+        int a = 10;
+        int b = 20;
+        int max = (a > b) ? a : b;
+        System.out.println(" maximum : " + max);
+    }
+}

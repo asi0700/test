4c8de6a43f34a1da59879b3c6519d1c2d39c5db9
asi0700 <asisanginov@gmail.com>
2026-09-04 13:43:50 +0300

    добавление сканера в файл max.java

--- max.java
+++ max.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
 public class max {
     public static void main(String[] args) {
-     
-        int a = 10;
-        int b = 20;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите 1 число: ");
+        int a = scanner.nextInt();
+        
+        System.out.print("Введите 2 число: ");
+        int b = scanner.nextInt();
+        
         int max = (a > b) ? a : b;
         System.out.println(" maximum : " + max);
     }
